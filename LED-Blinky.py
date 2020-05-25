b08d0f128f8095a3818cf8d906335a7c94e2557d
Retr0-Z <43178156+Retr0-Z@users.noreply.github.com>
2020-05-25 06:49:00 +0200

    Update LED-Blinky.py

--- LED-Blinky.py
+++ LED-Blinky.py
@@ -2,9 +2,12 @@ from gpiozero import LED
 from time import sleep
 
 led = LED(17)
+timeout = 60   # [seconds]
 
-while True:
-    led.on()
+timeout_start = time.time()
+
+while time.time() < timeout_start + timeout:  
+     led.on()
     sleep(1)
     led.off()
     sleep(1)
